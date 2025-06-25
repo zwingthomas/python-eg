e87e4ae507349fabb1ebb9836aed343a49d2564c
Thomas Zwinger <zwingthomas@gmail.com>
2025-06-25 11:39:53 -0500

    dataclasses and typing

---
+++ imports/Typing.py
@@ -0,0 +1,133 @@
+# Reasons to do type annotations
+# 1. Documentation
+# 2. Better autocomplete
+# 3. Other developers can read code easier
+# 4. Static code analysis tools will tell you if you have type mismatchs
+
+# x can store whatever type and be overwritten to any type
+# unlike other languages: int x = 1
+from typing import Any, Callable, Dict, List, Optional, Sequence, Set, Tuple, TypeVar
+
+
+x = 1
+x = "Thomas"
+
+# Does not enforce that x should be a string, does not trigger an error.
+# You need to use a static code analysis tool for this
+# pip install mypy
+# mypy [path-to-file]
+# with this you can run static code analysis
+x: str = 1
+
+# parameter types       # return type
+
+
+def add_numbers(a: int, b: int, c: int) -> int:
+    return a + b + c
+
+
+x: int = 0
+x = add_numbers(1, 2, 3)
+print(x)
+
+
+# Need to import List on python 3.8 or below to use it as a
+# subscriptable type.
+list_of_lists: List[List[int]] = []
+
+# Same for Dict
+dict: Dict[str, str] = {"a": "b"}
+
+hash_set: Set[int] = (1, 2,)
+
+
+# Store a type within a variable
+Vector = List[float]
+
+
+def foo(v: Vector) -> Vector:
+    pass
+
+
+# foo()  # type this out to show type hints
+
+Vectors = List[Vector]
+
+
+def foo(v: Vectors) -> Vector:
+    pass
+
+
+# foo()  # type this out to show type hints
+
+# Need to specify when types are Optional
+
+
+def foo(output: Optional[bool] = False):
+    pass
+
+
+# foo()  # type this out to show type hints
+
+# Declare this when you want to show you didn't just forget the type
+# you actually want to show it can be anything.
+
+
+def foo(output: Any):
+    pass
+
+
+# foo()  # type this out to show type hints
+
+
+# Sequence says that anything that can be indexed is fine
+def foo_seq(seq: Sequence[str]):
+    if isinstance(seq, Sequence):
+        for x in seq:
+            print(x)
+    else:
+        print("Not a sequence")
+    pass
+
+
+foo_seq(['a', 'b', 'c'])
+foo_seq(['a', 'b', 'c'])
+foo_seq("hello")
+foo_seq((i for i in range(100)))  # throws static code analysis error
+foo_seq(1)  # throws static code analysis error
+
+
+x: tuple = (1, 2, 3, "hello")
+x: Tuple[int, int, int] = (1, 2, 3)  # Need to declare every index!!
+
+#
+# Callable[[parameter, types, go, here], return type]
+#
+
+
+def foo(func: Callable[[int, int], int]):
+    func(1, 2)
+
+
+def foo() -> Callable[[int, int], int]:
+    func: Callable[[int, int], int] = lambda x, y: x + y
+    return func
+
+# TODO: Typing for lambdas
+
+#
+# Generics
+#
+
+
+# We don't know what type will be in the list, but it all must be the
+# same type.
+
+T = TypeVar('T')
+
+
+def get_item(lst: List[T], index: int) -> T:
+    return lst[index]
+
+
+# TODO: Generic classes
---
+++ language/classes/data_classes.py
@@ -0,0 +1,167 @@
+from dataclasses import InitVar, dataclass, field
+from typing import ClassVar, Optional
+
+#
+# This can be written in 4 lines of code
+#
+
+
+class Point:
+    x: int
+    y: int
+
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+    def __repr__(self):
+        x, y = self.x, self.y
+        return f"Point({x=}, {y=})"
+
+    def __eq__(self, other):
+        return self.x == other.x and self.y == other.y
+
+
+p1 = Point(1, 2)
+p2 = Point(2, 1)
+print(p1, p2)
+print(p1 == p2)
+
+
+#
+# This fills in a bunch of methods that we would otherwise have to
+# write. Added in version 3.7.
+#
+
+@dataclass
+class PointDataClass:
+    x: int
+    y: int
+
+
+p1 = PointDataClass(1, 2)
+p2 = PointDataClass(2, 1)
+print(p1, p2)
+print(p1 == p2)
+p1.x = 2
+p1.y = 1
+print(p1 == p2)
+
+
+class InventoryItem:
+
+    name: str
+    unit_price: float
+    quantity_on_hand: int = 0
+    # field
+    # 1. This is important to get a new list everytime, not a reference
+    # to an existing list
+    sizes: list[str] = field(default_factory=list)
+    # 2. New list can be set to default values.
+    # 3. init=Bool declares whether we want it to be included in the
+    # initialization.
+    sizes: list[str] = field(default=["medium"], init=False)
+    # ClassVar
+    # To declare class variables you need to give the type hint for
+    # ClassVar from the typing import
+    class_var: ClassVar[int] = 100
+
+    def total_cost(self) -> float:
+        return self.unit_price * self.quantity_on_hand
+
+    """
+    This is done automatically for us
+    """
+    # def __init__(self, name: str, unit_price: float, quantity_on_hand: int):
+    #     self.name = name
+    #     self.unit_price = unit_price
+    #     self.quantity_on_hand = quantity_on_hand
+
+
+# Notice how we only store a reference to the list rather than a new
+# list every time. This is solved by fields
+
+
+print("Default mutable variables only store references")
+
+
+def func(lst=[]):
+    lst.append(1)
+    print(lst)
+
+
+func()  # outputs: [1]
+func()  # outputs: [1, 1]
+
+
+print("Use Optional[TYPE] = None and then do a None check to assign")
+
+
+def func(lst: Optional[list[str]] = None):
+    if lst is None:
+        lst = []
+    lst.append(1)
+    print(lst)
+
+
+func()  # outputs: [1]
+func()  # outputs: [1]
+
+#
+# Inheriting from a non-dataclass to a dataclass
+#
+
+
+class Rectangle:
+    def __init__(self, height, width):
+        self.height = height
+        self.width = width
+
+
+@dataclass
+class Square(Rectangle):
+    # init automatically implements the wrapper, the repr, and the
+    # equals method
+    side: float
+
+    # Then it calls the post_init dunder method
+    def __post_init__(self):
+        super().__init__(self.side, self.side)
+
+
+#
+# Inheriting from a dataclass to a dataclass
+#
+
+@dataclass
+class Rectangle:
+    width: int
+    height: int
+
+
+@dataclass
+class ColoredRectangle(Rectangle):
+    color: str
+
+
+rect = ColoredRectangle(10, 10, "Green")
+
+
+#
+# Needing an initialization variable
+#
+
+@dataclass
+class C:
+    i: int
+    j: int | None = None
+    # InitVar will be included in the constructor, but not other methods
+    # and also that it will be passed to post_init when it is called.
+    database: InitVar[str | None] = None
+
+    def __post_init__(self, database):
+        if self.j is None and database is not None:
+            self.j = database.lookup('j')
+
+
+c = C(10, database={"j": "j-value"})
