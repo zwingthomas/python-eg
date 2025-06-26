dc8bcb9dfd77140e9ab12fc94b8f93045bc1ff34
Thomas Zwinger <zwingthomas@gmail.com>
2025-06-26 09:38:12 -0500

    abstract methods

--- language/classes/abstraction.py
+++ language/classes/abstraction.py
@@ -7,3 +7,78 @@ The benefits of these are:
 1. Prevents instantiation of the class itself
 2. Requires children to use inherited abstract methods
 """
+
+# abc stands for abstract base class
+from abc import ABC, abstractmethod
+
+
+class Vehicle(ABC):
+
+    @abstractmethod
+    def go(self):
+        pass
+
+    @abstractmethod
+    def stop(self):
+        pass
+
+
+try:
+    vehicle = Vehicle()
+except TypeError as e:
+    print(e)
+
+
+class Car_no_inheritance(Vehicle):
+    pass
+
+
+try:
+    car = Car_no_inheritance()
+except TypeError as e:
+    print(e)
+
+
+class Car(Vehicle):
+
+    def go(self):
+        print("Drive the car")
+
+    def stop(self):
+        print("Stop the car")
+
+
+car = Car()
+car.go()
+car.stop()
+
+
+class Motorcycle(Vehicle):
+
+    def go(self):
+        print("Ride the motorcycle")
+
+    def stop(self):
+        print("Stop the motorcycle")
+
+
+motorcycle = Motorcycle()
+motorcycle.go()
+motorcycle.stop()
+
+
+"""
+You must inherit every single abstract method!!
+"""
+
+
+class Boat(Vehicle):
+
+    def go(self):
+        print("Sail the board")
+
+
+try:
+    boat = Boat()
+except TypeError as e:
+    print(e)
